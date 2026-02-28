f2d69bcb2860f439aeaacbde79d59bd2cb705ca3
neonnskye <theneonnofficial@gmail.com>
2026-02-28 15:07:31 +0530

    First commit

---
+++ src/main.cpp
@@ -0,0 +1,54 @@
+#include <Arduino.h>
+#include <WiFiManager.h>
+#include <WiFiUdp.h>
+
+WiFiManager wifiManager;
+WiFiUDP wifiUDP;
+
+// put function declarations here:
+// int myFunction(int, int);
+
+void setup()
+{
+  // put your setup code here, to run once:
+  // int result = myFunction(2, 3);
+
+  Serial.begin(115200);
+  delay(1000);
+
+  // wifiManager.resetSettings(); // for debugging
+  wifiManager.autoConnect("Amrith's NodeMCU-32S", "pacman@123");
+
+  const int localPort = 5005;
+  wifiUDP.begin(localPort);
+  Serial.print("Listening on port: ");
+  Serial.println(localPort);
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+
+  int packetSize = wifiUDP.parsePacket();
+  if (packetSize)
+  {
+    Serial.print("Received packet of size: ");
+    Serial.println(packetSize);
+
+    char incoming[4096];
+    int len = wifiUDP.read(incoming, 4096);
+
+    if (len > 0)
+    {
+      incoming[len] = 0;
+    }
+
+    Serial.println("Packet received.");
+  }
+}
+
+// put function definitions here:
+// int myFunction(int x, int y)
+// {
+//   return x + y;
+// }
